55b3c629bbf764139a7909d65b6f8ee2f01d82ef
sumit <mazumdarsumit3@gmail.com>
2023-09-25 21:27:19 +0530

    Array functions

---
+++ 02-basics/02-arrays.js
@@ -0,0 +1,36 @@
+const marvel_heros = ['thor', 'Ironman', 'spiderman'];
+const dc_heros = ['superman', 'flash', 'batman'];
+
+// marvel_heros.push(dc_heros);
+// console.log(marvel_heros);
+
+// [ 'thor', 'Ironman', 'spiderman', [ 'superman', 'flash', 'batman' ] ]
+
+const allHeros = marvel_heros.concat(dc_heros);
+console.log(allHeros);
+// ['thor', 'Ironman', 'spiderman', 'superman', 'flash', 'batman'];
+
+const all_new_heros = [...marvel_heros, ...dc_heros];
+console.log(all_new_heros);
+// ['thor', 'Ironman', 'spiderman', 'superman', 'flash', 'batman'];
+
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]];
+
+const real_another_array = another_array.flat(Infinity);
+console.log(real_another_array);
+// [
+//   1, 2, 3, 4, 5,
+//   6, 7, 6, 7, 4,
+//   5
+// ]
+
+console.log(Array.isArray('Hero')); // false
+console.log(Array.from('Hero')); // [ 'H', 'e', 'r', 'o' ]
+console.log(Array.from({ name: 'hero' })); // []
+
+let score1 = 100;
+let score2 = 200;
+let score3 = 300;
+
+console.log(Array.of(score1, score2, score3));
+// [ 100, 200, 300 ]
